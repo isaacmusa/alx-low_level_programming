77b8d0e25d9ceaf8621375856b52342f53f5a309
isaacmusa <zicthngz@gmail.com>
2023-05-09 11:44:44 +0100

    Search_algo3

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -18,12 +18,12 @@ int linear_search(int *array, size_t size, int value){
 	if (array == NULL){
 		return -1;
 	}
-	for (i = 0; i < size; i++)
+	for (int i = 0; i < size; i++)
 	{
-		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
-		if (array[i] == value)
-			return (i);
+		printf("%d ", array[i]);
+		if (array[i] == value){
+			return i;
 	}
-
-	return (-1);
+    }
+	return -1;
 }
