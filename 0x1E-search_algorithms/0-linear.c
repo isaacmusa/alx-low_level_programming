aad7a34555a1fed8d91365cac217591c7357916f
isaacmusa <zicthngz@gmail.com>
2023-05-09 11:39:58 +0100

    Search_algo2

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -12,13 +12,12 @@
   *
   * Description: Prints a value every time it is compared in the array.
   */
-int linear_search(int *array, size_t size, int value)
-{
+int linear_search(int *array, size_t size, int value){
 	size_t i;
 
-	if (array == NULL)
-		return (-1);
-
+	if (array == NULL){
+		return -1;
+	}
 	for (i = 0; i < size; i++)
 	{
 		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
